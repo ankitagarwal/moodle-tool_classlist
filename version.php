d81dd907ea08154ad036f7a13ddab06331b31de4
Ankit Agarwal <ankit@moodle.com>
2013-09-13 11:21:18 +0800

    adding version.php and unique.js files

---
+++ version.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Classlist tool for Moodle
+//
+// Classlist is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Advanced Spam Cleaner is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// For a copy of the GNU General Public License, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin version info
+ *
+ * @package    tool_classlist
+ * @copyright  2013 Ankit Agarwal
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2013091300;
+$plugin->requires  = 2013061700;
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = "1.0";
+$plugin->component = 'tool_classlist'; // Full name of the plugin (used for diagnostics).
+
